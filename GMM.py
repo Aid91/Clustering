b478f4b650e7551f3405b3f4fb08ccc02e156dbb
Aid Ahmetovic <a.ahmetovic@netconomy.net>
2017-07-09 14:01:20 +0200

    Implementated initialization of algorithm parameters

--- GMM.py
+++ GMM.py
@@ -1,16 +1,41 @@
+import numpy as np
+import random
+
 class GMM:
-    def __init__(self, X):
+    def __init__(self, X, M):
         self.X = X
+        self.M = M
 
-        # initialize parameters for the Expectation Maximization algorithm
+        X_min = np.min(X[:, 0])
+        X_max = np.max(X[:, 0])
+
+        Y_min = np.min(X[:, 1])
+        Y_max = np.max(X[:, 1])
+
+        # initialization of the parameters needed for the Expectation Maximization algorithm
+        self.alpha_0 = np.ones((M, 1))
+        self.alpha_0 /= M
+
+        self.mu_0 = []
+        for i in range(self.M):
+            self.mu_0.append(np.array([np.random.uniform(X_min, X_max), np.random.uniform(Y_min, Y_max)]))
+
+        # initialize all covariances from covariance of the data
+        cov_of_data = np.cov(X.T)
+        self.sigma_0 = []
+        for t in range(self.M):
+            self.sigma_0.append(cov_of_data)
 
 
-        # initialize parameters for the K-means algorithm
+        # initialization of the parameters needed for the K-means algorithm
+        self.mu_kmeans_0 = random.sample(list(X), M)
 
-    def EM(X, M, max_iter, tol, interactive=False, diagonal=False):
+
+    def EM(max_iter, tol, interactive=False, diagonal=False):
+
         pass
 
-    def k_means(X, M, max_iter, tol, interactive=False):
+    def k_means(max_iter, tol, interactive=False):
         pass
 
     def sample(N):
