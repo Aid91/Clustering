cb7389e1f7e46b214354d68a1a45a353b0254d5d
Aid Ahmetovic <a.ahmetovic@netconomy.net>
2017-07-09 13:52:24 +0200

    Initial commit

---
+++ GMM.py
@@ -0,0 +1,17 @@
+class GMM:
+    def __init__(self, X):
+        self.X = X
+
+        # initialize parameters for the Expectation Maximization algorithm
+
+
+        # initialize parameters for the K-means algorithm
+
+    def EM(X, M, max_iter, tol, interactive=False, diagonal=False):
+        pass
+
+    def k_means(X, M, max_iter, tol, interactive=False):
+        pass
+
+    def sample(N):
+        pass
